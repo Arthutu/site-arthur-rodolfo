96cf2295dbecf834610815af068f08e96288a3bc
Arthur Rodolfo <arthurrodolfo81@gmail.com>
2023-09-17 11:54:42 -0700

    Adds i18n support for English and Brazilian Portuguese

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+    en: "English",
+    ptBR:  "Português (Brasil)"
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+    en: {
+        "nav.about": "About",
+        "nav.experience": "Experience",
+        "nav.projects": "Projects",
+        "footer.developedWith": "Developed with",
+        "footer.hostedOn": "Hosted on",
+        "theme.dark": "Dark theme",
+    },
+    ptBR: {
+        "nav.about": "Sobre",
+        "nav.experience": "Experiência",
+        "nav.projects": "Projetos",
+        "footer.developedWith": "Desenvolvido com",
+        "footer.hostedOn": "Hospedado em",
+        "theme.dark": "Tema escuro",
+    },
+} as const;
