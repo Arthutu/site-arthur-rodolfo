329bb2762953f6186526f3fb0ab425a0c774a5e0
Arthur Rodolfo <arthurrodolfo81@gmail.com>
2023-09-17 17:24:22 -0700

    Add custom 404 page

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -15,6 +15,8 @@ export const ui = {
         "footer.developedWith": "Developed with",
         "footer.hostedOn": "Hosted on",
         "theme.dark": "Dark theme",
+        "404.title": "Page not found",
+        "404.description": "Ops, we could not find",
     },
     ptBR: {
         "nav.about": "Sobre",
@@ -23,6 +25,8 @@ export const ui = {
         "footer.developedWith": "Desenvolvido com",
         "footer.hostedOn": "Hospedado em",
         "theme.dark": "Tema escuro",
+        "404.title": "Página não encontrada",
+        "404.description": "Ops, não conseguimos encontrar",
     },
     fr: {
         "nav.about": "À propos",
@@ -31,6 +35,8 @@ export const ui = {
         "footer.developedWith": "Développé avec",
         "footer.hostedOn": "Hébergé sur",
         "theme.dark": "Thème sombre",
+        "404.title": "Page non trouvée",
+        "404.description": "Ops, nous n'avons pas pu trouver",
     },
     es: {
         "nav.about": "Sobre",
@@ -39,5 +45,7 @@ export const ui = {
         "footer.developedWith": "Desarrollado con",
         "footer.hostedOn": "Hospedado en",
         "theme.dark": "Tema oscuro",
+        "404.title": "Página no encontrada",
+        "404.description": "Ops, no pudimos encontrar",
     },
 } as const;
