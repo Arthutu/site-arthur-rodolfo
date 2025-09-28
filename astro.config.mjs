afce23e1999d23000a26d66b990b88a68217e21c
Arthur <arthurrodolfo81@gmail.com>
2025-09-28 10:45:43 -0700

    remove astro tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 import vercel from "@astrojs/vercel";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), icon()],
-    output: "server",
-    adapter: vercel({
-        webAnalytics: { enabled: true },
-        speedInsights: { enabled: true },
-    }),
-    trailingSlash: "never"
+  integrations: [icon()],
+  output: "server",
+
+  adapter: vercel({
+      webAnalytics: { enabled: true },
+      speedInsights: { enabled: true },
+  }),
+
+  trailingSlash: "never",
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
