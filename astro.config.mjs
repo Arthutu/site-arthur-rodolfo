ea3df07790b7db1fa8cf46de8cf268ed486aa411
Arthur <arthurrodolfo81@gmail.com>
2025-11-09 17:13:31 -0800

    attempt to fix deploy with cloud flare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
   output: "static",
-  adapter: cloudflare({
-    imageService: "compile"
-  }),
   trailingSlash: "never",
   vite: {
     plugins: [tailwindcss()]
